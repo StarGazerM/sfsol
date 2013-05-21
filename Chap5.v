cf63d17f69d788e5b56357f3f464be6e4ee48649
Satyender Yadav <satyenderyadav4993@gmail.com>
2013-05-22 03:16:00 +0530

    Till optional Material Done in chapter 5

--- Chap5.v
+++ Chap5.v
@@ -260,3 +260,165 @@ Theorem SSev__even : forall n,
 Proof.
   intros n E. inversion E as [| n' E']. apply E'. Qed.
 
+Theorem SSSSev__even : forall n,
+  ev (S (S (S (S n)))) -> ev n.
+Proof.
+  intros n E. inversion E as [| n' E']. inversion E' as [| n'' E''].
+  apply E''. Qed.
+
+Theorem even5_nonsense :
+  ev 5 -> 2 + 2 = 9.
+Proof.
+  intros E. inversion E. inversion H0. inversion H2. Qed.
+
+Theorem ev_minus2': forall n,
+  ev n -> ev (pred (pred n)).
+Proof.
+  intros n E. inversion E as [| n' E'].
+  Case "E = ev_0". simpl. apply ev_0.
+  Case "E = ev_SS n' E'". simpl. apply E'. Qed.
+
+Theorem ev_ev__ev : forall n m,
+  ev (n+m) -> ev n -> ev m.
+Proof.
+  intros n m E1 E2.
+  induction E2 as [| n' E2'].
+  simpl in E1. apply E1.
+  simpl in E1.
+  inversion E1.
+  apply IHE2'.
+  apply H0.
+  Qed.
+
+Theorem ev_plus_plus : forall n m p,
+  ev (n+m) -> ev (n+p) -> ev (m+p).
+Proof.
+  assert(forall n m p,n + m + n + p = double n + m + p).
+    intros.
+    induction n.
+    simpl. rewrite -> plus_O_r. reflexivity.
+    simpl. rewrite <- plus_n_Sm. rewrite <- IHn.
+    simpl. reflexivity.
+  assert (forall n m, ev(double n + m) -> ev m).
+    intros.
+    induction n.
+    simpl in H0. apply H0.
+    simpl in H0. inversion H0 as [| m' H1]. apply IHn. apply H2.
+  intros n m p E1 E2.
+  assert (ev (n + m + n + p)).
+    replace (n + m + n + p) with (n + m + (n + p)).
+    apply ev_sum. apply E1. apply E2.
+    apply plus_assoc.
+  rewrite H in H1. rewrite <- plus_assoc in H1. apply H0 in H1.
+  apply H1.
+  Qed.
+
+Theorem plus_2_2_is_4 :
+  2 + 2 = 4.
+Proof. reflexivity. Qed.
+
+Definition plus_fact : Prop := 2 + 2 = 4.
+Check plus_fact.
+
+Theorem plus_fact_is_true :
+  plus_fact.
+Proof. reflexivity. Qed.
+
+Definition strange_prop1 : Prop :=
+  (2 + 2 = 5) -> (99 + 26 = 42).
+
+Definition strange_prop2 :=
+  forall n, (ble_nat n 17 = true) -> (ble_nat n 99 = true).
+
+Definition between (n m o: nat) : Prop :=
+  andb (ble_nat n o) (ble_nat o m) = true.
+
+Definition teen : nat->Prop := between 13 19.
+
+Definition true_for_zero (P:nat->Prop) : Prop :=
+  P 0.
+
+Definition true_for_all_numbers (P:nat->Prop) : Prop :=
+  forall n, P n.
+
+
+Definition preserved_by_S (P:nat->Prop) : Prop :=
+  forall n', P n' -> P (S n').
+
+Check nat_ind.
+
+Theorem mult_0_r' : forall n:nat,
+  n * 0 = 0.
+Proof.
+  apply nat_ind.
+  Case "O". reflexivity.
+  Case "S". simpl. intros n IHn. rewrite -> IHn.
+    reflexivity. Qed.
+
+Theorem plus_one_r' : forall n:nat,
+  n + 1 = S n.
+Proof.
+  apply nat_ind.
+  reflexivity.
+  intros. simpl. rewrite -> H. reflexivity.
+  Qed.
+
+Inductive rgb : Type :=
+  | red : rgb
+  | green : rgb
+  | blue : rgb.
+Check rgb_ind.
+
+Inductive natlist : Type :=
+  | nnil : natlist
+  | ncons : nat -> natlist -> natlist.
+
+Check natlist_ind.
+
+Inductive ExSet : Type :=
+  | con1 : bool -> ExSet
+  | con2 : nat -> ExSet -> ExSet.
+
+Check ExSet_ind.
+
+Inductive tree (X:Type) : Type :=
+  | leaf : X -> tree X
+  | node : tree X -> tree X -> tree X.
+Check tree_ind.
+
+Inductive mytype (X:Type) : Type :=
+  | constr1 : X -> mytype X
+  | constr2 : nat -> mytype X
+  | constr3 : mytype X -> nat -> mytype X.
+
+Check mytype_ind.
+
+Inductive foo (X Y:Type) : Type :=
+  | bar : X -> foo X Y
+  | baz : Y -> foo X Y
+  | quux : (nat -> foo X Y) -> foo X Y.
+
+Check foo_ind.
+
+Inductive foo' (X:Type) : Type :=
+  | C1 : list X -> foo' X -> foo' X
+  | C2 : foo' X.
+
+Check foo'_ind.
+
+Definition P_m0r (n:nat) : Prop :=
+  n * 0 = 0.
+
+Definition P_m0r' : nat->Prop :=
+  fun n => n * 0 = 0.
+
+Theorem mult_0_r'' : forall n:nat,
+  P_m0r n.
+Proof.
+  apply nat_ind.
+  Case "n = O". reflexivity.
+  Case "n = S n'".
+    (* Note the proof state at this point! *)
+    unfold P_m0r. simpl. intros n' IHn'.
+    apply IHn'. Qed.
+
